9aa60e5970d4638c45fecbf2bd9c0414876762c4
Dan Nilsson <dan.nilsson2@gmail.com>
2015-07-24 13:12:46 +0200

    first commit of drawing application

---
+++ src/webgl_draw.js
@@ -0,0 +1,103 @@
+"use strict";
+
+var canvas;
+var gl;
+
+var points = [];
+var segments=[];
+
+var fColor;
+const black = vec4(0.0, 0.0, 0.0, 1.0);
+const red = vec4(1.0, 0.0, 0.0, 1.0);
+var bufferId;
+var drawing=false;
+    
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    canvas.addEventListener("mousedown",mousePressed);
+    canvas.addEventListener("mouseup",mouseReleased);
+    canvas.addEventListener("mousemove",mouseMove);
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
+
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+
+    bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    // gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+
+    fColor = gl.getUniformLocation(program,"fColor");
+     
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+function mouseReleased(){
+  console.log("mouseRelease");
+  drawing=false;
+}
+
+function mouseMove(event){
+  if (drawing) {
+    var t = toCanvasCoord(event);
+    draw(t);
+ //   console.log("mouseMove: ", t);
+  }
+}
+
+function draw(point){
+  var points=segments[segments.length-1];
+  points.push(point);  
+ 
+}
+
+function mousePressed(event){
+  var points=[];
+  var point=toCanvasCoord(event);
+  points.push(point);
+  segments.push(points);
+  drawing=true;
+}
+
+  function toCanvasCoord(event) {
+    return vec2(-1 + 2 * event.clientX / canvas.width, -1 + 2 * (canvas.height - event.clientY) / canvas.height);
+  }
+
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.uniform4fv(fColor, flatten(black));
+  
+  for (var i = 0; i < segments.length; i++) {
+     var points = segments[i];
+     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+     gl.drawArrays(gl.LINE_STRIP, 0, points.length);
+  }
+  
+
+  requestAnimFrame(render);
+}
